273324462b88a800a0b3f467fdc0e73cf50a5f59
Jae Yong Lee <leejaeyong7@gmail.com>
2023-04-21 12:38:29 -0500

    updating scripts

--- scripts/train_eth3d_full.sh
+++ scripts/train_eth3d_full.sh
@@ -1,6 +1,6 @@
 MOUNT_DIR="/home/lee896/data/$(hostname -s)"
 DATA_DIR="${MOUNT_DIR}/eth3d_processed"
-CHECKPOINT_DIR="/${MOUNT_DIR}/eth3d_outputs"
+CHECKPOINT_DIR="/${MOUNT_DIR}/eth3d_outputs_regnerf_full"
 mkdir -p $CHECKPOINT_DIR
 scene=$1
 rm -r "${CHECKPOINT_DIR}/${scene}/"
@@ -9,4 +9,4 @@ python -m write --gin_configs=configs/eth3d_full.gin --gin_bindings="Config.data
 
 # after training, upload results to jyl.kr
 zip -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip ${CHECKPOINT_DIR}/${scene}/results
-scp -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip jyl:/mnt/data1/cluster_results/regnerf/
+scp -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip jyl:/mnt/data1/cluster_results/regnerf_full/
