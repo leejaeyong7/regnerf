3a777098241113b9b9d19169366badc6ed922dde
Jae Yong Lee <leejaeyong7@gmail.com>
2023-04-21 12:26:48 -0500

    adding full training

---
+++ scripts/train_eth3d_full.sh
@@ -0,0 +1,12 @@
+MOUNT_DIR="/home/lee896/data/$(hostname -s)"
+DATA_DIR="${MOUNT_DIR}/eth3d_processed"
+CHECKPOINT_DIR="/${MOUNT_DIR}/eth3d_outputs"
+mkdir -p $CHECKPOINT_DIR
+scene=$1
+rm -r "${CHECKPOINT_DIR}/${scene}/"
+python -m train --gin_configs=configs/eth3d_full.gin --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'"
+python -m write --gin_configs=configs/eth3d_full.gin --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'"
+
+# after training, upload results to jyl.kr
+zip -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip ${CHECKPOINT_DIR}/${scene}/results
+scp -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip jyl:/mnt/data1/cluster_results/regnerf/
