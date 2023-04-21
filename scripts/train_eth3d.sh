9fadf9899b1b019e43e65e8d6eaa397e4a1d43ab
Jae Yong Lee <jae.lee@reconstructinc.com>
2023-04-21 14:32:56 -0500

    updating full running script

--- scripts/train_eth3d.sh
+++ scripts/train_eth3d.sh
@@ -4,8 +4,16 @@ CHECKPOINT_DIR="/${MOUNT_DIR}/eth3d_outputs_regnerf"
 mkdir -p $CHECKPOINT_DIR
 scene=$1
 rm -r "${CHECKPOINT_DIR}/${scene}/"
-python -m train --gin_configs=configs/eth3d.gin --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'"
-python -m write --gin_configs=configs/eth3d.gin --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'"
+
+python -m train --gin_configs=configs/eth3d.gin \
+                --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" \
+                --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'" \
+                --gin_bindings="Config.full_training = False"
+
+python -m write --gin_configs=configs/eth3d.gin \
+                --gin_bindings="Config.data_dir = '${DATA_DIR}/${scene}/'" \
+                --gin_bindings="Config.checkpoint_dir = '${CHECKPOINT_DIR}/${scene}/'" \
+                --gin_bindings="Config.full_training = False"
 
 # after training, upload results to jyl.kr
 zip -r ${CHECKPOINT_DIR}/${scene}/${scene}.zip ${CHECKPOINT_DIR}/${scene}/results
